e1b8b6f37e77893cdc1242bdee6459ab2bca25fc
Liberate99 <1186400034@qq.com>
2018-05-06 15:01:10 +0800

    .

--- BookTrading/store/StoreViewController.swift
+++ BookTrading/store/StoreViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class StoreViewController: UIViewController {
 
+    
+    @IBOutlet weak var recommendScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // statusBar
         UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent;
+        
+        // navigater
         self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.9)
         let dict:NSDictionary = [NSAttributedStringKey.foregroundColor: UIColor.white,NSAttributedStringKey.font: UIFont.systemFont(ofSize: 19, weight: UIFont.Weight.light)]
         self.navigationController?.navigationBar.titleTextAttributes = dict as? [NSAttributedStringKey : AnyObject]
+        
+        // scrollView
+        recommendScrollView.backgroundColor = UIColor.green
+        recommendScrollView.isPagingEnabled = true
+        recommendScrollView.contentSize = CGSize(width: (self.view.frame.size.width)*3, height: (self.view.frame.size.height)/3)
+        recommendScrollView.showsVerticalScrollIndicator = false
+        recommendScrollView.showsHorizontalScrollIndicator = false
+        recommendScrollView.scrollsToTop = false
+        let numOfPages = 3
+        for i in 0...numOfPages{
+            var <#name#> = <#value#>
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
