0ad75ede8978d8abe6032fddf1b1460c08c3675d
Liberate99 <1186400034@qq.com>
2018-05-08 02:55:05 +0800

    完善MineViewController  完善度：20%

--- BookTrading/mine/MineViewController.swift
+++ BookTrading/mine/MineViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class MineViewController: UIViewController {
 
+    @IBOutlet var backGroundView: UIView!
+    @IBOutlet weak var userUnderView: UIView!
+    @IBOutlet weak var userPic: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userIntro: UILabel!
+    @IBOutlet weak var myGoods: UIButton!
+    @IBOutlet weak var myCollection: UIButton!
+    @IBOutlet weak var liked: UIButton!
+    @IBOutlet weak var setting: UIButton!
+    
+    // myGoods tapped
+    @objc func myGoodsTapped(_ button:UIButton){
+        print("我的发布")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        
+        // myGoods
+        myGoods.backgroundColor = UIColor.white
+        myGoods.addTarget(self, action: #selector(myGoodsTapped(_:)), for: .touchUpInside)
+        
+        // myCollection
+        myCollection.backgroundColor = UIColor.white
+        
+        // liked
+        liked.backgroundColor = UIColor.white
+        
+        // setting
+        setting.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
         UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent;
         self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.9)
