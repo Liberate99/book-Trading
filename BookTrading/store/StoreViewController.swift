cb9c65dce4c8d86a86872a0e320ec2a9da1f6bba
Liberate99 <1186400034@qq.com>
2018-05-08 20:30:47 +0800

    重构StoreViewController

--- BookTrading/store/StoreViewController.swift
+++ BookTrading/store/StoreViewController.swift
@@ -1,11 +1,3 @@
-//
-//  StoreViewController.swift
-//  BookTrading
-//
-//  Created by apple on 2018/4/27.
-//  Copyright © 2018年 Liberate. All rights reserved.
-//
-
 import UIKit
 
 extension UIImage {
@@ -21,119 +13,58 @@ extension UIImage {
     }
 }
 
-class StoreViewController: UIViewController, UIScrollViewDelegate {
-
-    
-    @IBOutlet weak var recommendScrollView: UIScrollView!
-    @IBOutlet weak var pageControl: UIPageControl!
-    // 传记
-    @IBOutlet weak var biography: UIButton!
-    // 杂志
-    @IBOutlet weak var magazine: UIButton!
-    // 生活
-    @IBOutlet weak var livelihood: UIButton!
-    // 摄影
-    @IBOutlet weak var photography: UIButton!
-    // 教育
-    @IBOutlet weak var education: UIButton!
-    // 绘画
-    @IBOutlet weak var drawing: UIButton!
-    // 音乐
-    @IBOutlet weak var music: UIButton!
-    // 其他
-    @IBOutlet weak var moreKind: UIButton!
+class StoreViewController: UIViewController,UIScrollViewDelegate {
     
     var courses = [["title":"《1Q84》",         "content":"《傲慢与偏见》是简·奥斯汀的代表作。小说讲述了乡绅之女伊丽莎白·班内特的爱情故事。",    "pic":"1Q84.png"],
                    ["title":"《傲慢与偏见》",     "content":"《傲慢与偏见》是简·奥斯汀的代表作。小说讲述了乡绅之女伊丽莎白·班内特的爱情故事。",    "pic":"傲慢与偏见.png"],
                    ["title":"《霍乱时期的爱情》",  "content":"《霍乱时期的爱情》是加西亚·马尔克斯获得诺贝尔文学奖后出版的第一部小说，讲述了一段跨越半个多世纪的爱情史诗，穷尽了所有爱情的可能性：忠贞的、隐秘的、粗暴的、羞怯的、柏拉图式的、放荡的、转瞬即逝的、生死相依的……再现了时光的无情流逝，被誉为“人类有史以来最伟大的爱情小说”，是20世纪最重要的经典文学巨著之一。",  "pic":"霍乱时期的爱情.png"]]
     
-    //颜色渐变
-    func turquoiseColor() -> CAGradientLayer {
-        let topColor = UIColor.clear
-        let bottomColor = UIColor.black.withAlphaComponent(0.7)
-        
-        //let gradientColors: Array <AnyObject> = [topColor.cgColor, bottomColor.cgColor]
-        let gradientColors = [topColor.cgColor, bottomColor.cgColor]
-        
-        //let gradientLocations: Array <AnyObject> = [0.0 as AnyObject, 1.0 as AnyObject]
-        let gradientLocations:[NSNumber] = [0.0, 1.0]
-        
-        let gradientLayer: CAGradientLayer = CAGradientLayer()
-        gradientLayer.colors = gradientColors
-        gradientLayer.locations = gradientLocations as? [NSNumber]
-        
-        return gradientLayer
-    }
     
-    // UIScrollViewDelegate方法，每次滚动结束后调用
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        // 通过scrollView内容的偏移量计算当前显示的是第几页
-        let page = Int(scrollView.contentOffset.x/scrollView.frame.size.width)
-        
-        // 设置pageController的当前页
-        pageControl.currentPage = page
-    }
-    
-    // 点击页控件时事件处理
-    func pageChange(sender: UIPageControl){
-        
-        // 根据点击的页数，计算scrollView需要显示的偏移量
-        var frame = recommendScrollView.frame
-        frame.origin.x = frame.size.width * CGFloat(sender.currentPage)
-        frame.origin.y = 0
-        
-        // 展现当前页面内容
-        recommendScrollView.scrollRectToVisible(frame, animated: true)
-    }
+    let scrollView = UIScrollView(frame: CGRect(x:0, y:64, width:UIScreen.main.bounds.width,height: 230))
+    var colors:[UIColor] = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow]
+    var frame: CGRect = CGRect(x:0, y:0, width:0, height:0)
+    var pageControl : UIPageControl = UIPageControl(frame: CGRect(x:0,y: 244, width:UIScreen.main.bounds.width, height:50))
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // statusBar
+        //statusBar
         UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent;
         
         // navigater
         self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.9)
         let dict:NSDictionary = [NSAttributedStringKey.foregroundColor: UIColor.white,NSAttributedStringKey.font: UIFont.systemFont(ofSize: 19, weight: UIFont.Weight.light)]
         self.navigationController?.navigationBar.titleTextAttributes = dict as? [NSAttributedStringKey : AnyObject]
+        // Do any additional setup after loading the view, typically from a nib.
         
-        /*
-         * scrollView && pageControl
-         */
+        scrollView.delegate = self
+        
         // scrollView 不回弹
-        recommendScrollView.bounces = false
-        
-        // 尺寸
-        recommendScrollView.contentSize = CGSize(width: 375*CGFloat(self.courses.count), height: 230)
-        
-        //pageControl
-        pageControl.center = CGPoint(x: UIScreen.main.bounds.width/2, y: 270)
-        
-        // 关闭滚动显示条
-        recommendScrollView.showsHorizontalScrollIndicator = false
-//        recommendScrollView.showsVerticalScrollIndicator = false
-//        recommendScrollView.scrollsToTop = false
-        
-        // 协议代理，在本类中处理滚动事件
-        recommendScrollView.delegate = self
+        scrollView.bounces = false
         
         // 滚动时只能停留到某一页
-        recommendScrollView.isPagingEnabled = true
+        scrollView.isPagingEnabled = true
         
-        // 添加页面到滚动页面里
-        let size = recommendScrollView.bounds.size
+        // 关闭滚动显示条
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        self.view.addSubview(scrollView)
+        
+        
         for (seq,course) in courses.enumerated() {
-            let page = UIImageView(frame: CGRect(x: CGFloat(seq)*size.width, y: 0, width: 375, height: 230))
-            let _reSize = CGSize(width: 375, height: 230)
-            page.image = UIImage(named: course["pic"]!)!.reSizeImage(reSize: _reSize)
-            recommendScrollView.addSubview(page)
             
-            //let GradientView = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 230))
+            frame.origin.x = self.scrollView.frame.size.width * CGFloat(seq)
+            frame.size = self.scrollView.frame.size
+            
+            let subView = UIImageView(frame: frame)
+            //subView.backgroundColor = colors[index]
+            subView.image = UIImage(named: course["pic"]!)
+            //!.reSizeImage(reSize: reSize)
+            self.scrollView .addSubview(subView)
+            
             let background = turquoiseColor()
             background.frame = CGRect(x:0, y:0, width:UIScreen.main.bounds.width , height: 230)
-            page.layer.insertSublayer(background, at: 0)
+            subView.layer.insertSublayer(background, at: 0)
             
             let title = UILabel(frame: CGRect(x: 0, y: 100, width: 375, height: 50))
             title.text = course["title"]
@@ -141,7 +72,7 @@ class StoreViewController: UIViewController, UIScrollViewDelegate {
             title.font = UIFont(name: "Helvetica", size: 20)
             title.textColor = UIColor.white
             title.textAlignment = NSTextAlignment.center
-            page.addSubview(title)
+            subView.addSubview(title)
             
             let content = UILabel(frame: CGRect(x: 30, y: 120, width: 315, height: 100))
             //content.text = course["content"]
@@ -161,25 +92,67 @@ class StoreViewController: UIViewController, UIScrollViewDelegate {
                               NSAttributedStringKey.paragraphStyle: paraph]
             content.attributedText = NSAttributedString(string: course["content"]!, attributes: attributes)
             content.numberOfLines = 2
-            page.addSubview(content)
+            subView.addSubview(content)
         }
         
-        // 页控件属性
-        pageControl.numberOfPages = courses.count
-        pageControl.currentPage = 0
+        self.scrollView.contentSize = CGSize(width:self.scrollView.frame.size.width * CGFloat(self.courses.count),height: self.scrollView.frame.size.height)
+        pageControl.addTarget(self, action: #selector(self.changePage(sender:)), for: UIControlEvents.valueChanged)
         
-        // 添加页控制器
-        self.view.addSubview(pageControl)
+        // 设置pageControl
+        configurePageControl()
         
-        //设置页空间点击事件
-        pageControl.addTarget(self, action: Selector(("pageChanged")), for: UIControlEvents.valueChanged)
-        
+        // 设置页控件点击事件
         
     }
-
+    
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        self.pageControl.numberOfPages = courses.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.red
+        self.pageControl.pageIndicatorTintColor = UIColor.black
+        self.pageControl.currentPageIndicatorTintColor = UIColor.green
+        self.view.addSubview(pageControl)
+    }
+    
+    // MARK : TO CHANGE WHILE CLICKING ON PAGE CONTROL
+    @objc func changePage(sender: AnyObject) -> () {
+        let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
+        scrollView.setContentOffset(CGPoint(x:x, y:0), animated: true)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = Int(pageNumber)
+    }
+    
+    //颜色渐变
+    func turquoiseColor() -> CAGradientLayer {
+        let topColor = UIColor.clear
+        let bottomColor = UIColor.black.withAlphaComponent(0.7)
+        
+        //let gradientColors: Array <AnyObject> = [topColor.cgColor, bottomColor.cgColor]
+        let gradientColors = [topColor.cgColor, bottomColor.cgColor]
+        
+        //let gradientLocations: Array <AnyObject> = [0.0 as AnyObject, 1.0 as AnyObject]
+        let gradientLocations:[NSNumber] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as? [NSNumber]
+        
+        return gradientLayer
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
+
+
+
