902fb037b9bf4fcec5ca3294e0ba1518568bb663
Liberate99 <1186400034@qq.com>
2018-05-13 04:26:48 +0800

    book

---
+++ BookTrading/register/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  BookTrading
+//
+//  Created by apple on 2018/5/13.
+//  Copyright © 2018年 Liberate. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
