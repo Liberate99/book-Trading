c55227e922d49e0d921a9aa82bcc89924355adec
Liberate99 <1186400034@qq.com>
2018-05-25 16:38:37 +0800

    完善用户注册功能2

--- BookTrading/register/RegisterViewController.swift
+++ BookTrading/register/RegisterViewController.swift
@@ -56,6 +56,62 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         print(username.text)
         print(password.text)
         print(password2.text)
+        
+        if (username.text! != ""){
+            if (password.text! != ""){
+                if (password2.text! != ""){
+                    if (password.text! == password2.text!){
+                        let _parameter = ["username":username.text!] as [String : Any]
+                        Alamofire.request("http://127.0.0.1:8080/user/selectUserByName?", method: .get, parameters: _parameter, encoding: URLEncoding.default)
+                            .validate()
+                            .responseJSON{
+                                (response) in
+                                // 有错误就打印错误，没有就解析数据
+                                if let Error = response.result.error{
+                                    print(Error)
+                                } else if let jsonresult = response.result.value {
+                                    // 用 SwiftyJSON 解析数据
+                                    let data = JSON(jsonresult)
+                                    print(data)
+                                    if data == [] {
+                                        print("账号可以写入数据库")
+                                        self.addUser(username: self.username.text!, password: self.password.text!)
+                                    }else{
+                                        SwiftNotice.showText("该用户名已被占用！")
+                                        print("用户名不可用")
+                                    }
+                                }
+                        }
+                    }else{ SwiftNotice.showText("密码不一致！")}
+                }else{ SwiftNotice.showText("请确认密码！") }
+            }else{ SwiftNotice.showText("请输入密码！") }
+        }else{ SwiftNotice.showText("请输入用户名！") }
+    }
+    
+    func addUser(username: String, password: String){
+        print(username)
+        print(password)
+        let _parameter = ["username":username,"password": password] as [String : Any]
+        
+        Alamofire.request("http://127.0.0.1:8080/user/addUser?", method: .post, parameters: _parameter, encoding: URLEncoding.default)
+            .validate()
+            .responseJSON{
+                (response) in
+                // 有错误就打印错误，没有就解析数据
+                if let Error = response.result.error{
+                    print(Error)
+                } else if let jsonresult = response.result.value {
+                    // 用 SwiftyJSON 解析数据
+                    let data = JSON(jsonresult)
+                    print(data)
+                    if data == 1 {
+                        SwiftNotice.showText("注册成功！")
+                        self.dismiss(animated: true, completion: nil)
+                    }else{
+                        SwiftNotice.showText("注册失败！")
+                    }
+                }
+            }
     }
     
     @IBAction func backToLogin(_ sender: Any) {
